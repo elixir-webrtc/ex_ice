9f0bd27f1eb0d191af9981e338212656a04090d8
Michał Śledź <michalsledz34@gmail.com>
2022-07-24 13:55:26 +0200

    Initial commit

---
+++ lib/ex_ice.ex
@@ -0,0 +1,18 @@
+defmodule ExIce do
+  @moduledoc """
+  Documentation for `ExIce`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ExIce.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ex_ice_test.exs
@@ -0,0 +1,8 @@
+defmodule ExIceTest do
+  use ExUnit.Case
+  doctest ExIce
+
+  test "greets the world" do
+    assert ExIce.hello() == :world
+  end
+end
