c44874e9ece3db9571a34c3c05ab9e1c416a38c9
João Thallis <joaothallis@turn.io>
2026-04-29 09:28:10 -0300

    chore: use turnhub ex_turn

--- mix.exs
+++ mix.exs
@@ -56,10 +56,7 @@ defmodule ExICE.MixProject do
   defp deps do
     [
       {:ex_stun, "~> 0.2.0"},
-      {:ex_turn,
-       git: "https://github.com/joaothallis/ex_turn.git",
-       ref: "8a4801b2423bfcc52fe2a5ffb35472324a066556",
-       override: true},
+      {:ex_turn, github: "turnhub/ex_turn", branch: "master"},
       {:elixir_uuid, "~> 1.0"},
       {:excoveralls, "~> 0.15", only: :test, runtime: false},
       {:ex_doc, "~> 0.27", only: :dev, runtime: false},
